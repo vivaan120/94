bca5f0ff3eac426379ba203f6d453792ede2e544
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-07-30 10:38:43 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB7DwKlxGvckWScMCeooXCjc4uOySLGn6I",
+    authDomain: "let-s-chat-2dc93.firebaseapp.com",
+    databaseURL: "https://let-s-chat-2dc93-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-2dc93",
+    storageBucket: "let-s-chat-2dc93.appspot.com",
+    messagingSenderId: "51185969473",
+    appId: "1:51185969473:web:c4eebc2d6fd840b38a59c0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
